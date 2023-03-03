a20cf5cd9824aaca41d8ce575172a7051272e014
AndrewPautov <apautov04@gmail.com>
2023-03-03 16:05:59 +0300

    All work done

--- MVVM Example/Camera/ViewController.swift
+++ MVVM Example/Camera/ViewController.swift
@@ -8,13 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-     
+    @IBOutlet private var flashButton: UIButton!
+    @IBOutlet private var livePhotoButton: UIButton!
+    
+    var viewModel: CameraViewModelProtocol = CameraViewModel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureFlashButton()
+        configureLivePhotoButton()
+    }
+    
+    @IBAction func didTapFlashButton() {
+        viewModel.isFlashEnabled = !viewModel.isFlashEnabled
+        configureFlashButton()
     }
 
-
+    @IBAction func didTapLivePhotoButton() {
+        viewModel.isLivePhotoEnabled = !viewModel.isLivePhotoEnabled
+        configureLivePhotoButton()
+    }
+    
+    private func configureFlashButton() {
+        let systemName = viewModel.isFlashEnabled ? "lightbulb" : "lightbult.slash"
+        flashButton.setImage(UIImage(systemName: systemName), for: .normal)
+    }
+    
+    private func configureLivePhotoButton() {
+        let systemName = viewModel.isLivePhotoEnabled ? "livephoto" : "livephoto.slash"
+        livePhotoButton.setImage(UIImage(systemName: systemName), for: .normal)
+    }
 }
 
